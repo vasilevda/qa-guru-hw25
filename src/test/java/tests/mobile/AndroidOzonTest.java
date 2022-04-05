b541ac2cd087a76c5aa4bee3bf11d3442879f232
da.vasilyev <dmitry.vasilyew@gmail.com>
2022-04-05 21:15:44 +0300

    fix

--- src/test/java/tests/mobile/AndroidOzonTest.java
+++ src/test/java/tests/mobile/AndroidOzonTest.java
@@ -1,7 +1,5 @@
 package tests.mobile;
 
-import com.codeborne.selenide.Selenide;
-import io.appium.java_client.AppiumBy;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
@@ -11,104 +9,52 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import tests.TestBase;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.back;
-import static io.qameta.allure.Allure.step;
+import tests.mobile.steps.StepsMobile;
 
 @Tag("Mobile")
 @Epic("Android")
 @Owner("vasilevda")
 public class AndroidOzonTest extends TestBase {
+    private StepsMobile stepsMobile = new StepsMobile();
 
     @Test
     @DisplayName("Тестирование поиска")
     void searchAppTest() {
-        step("Нажать на кнопку <Каталог>", () ->
-                $(AppiumBy.id("ru.ozon.app.android:id/menu_catalog")).click());
-
-        step("Проверить наличие строки <Поиск>", () -> {
-            Selenide.sleep(2000);
-            if ($(AppiumBy.accessibilityId("widget common.pageHeader")).isDisplayed())
-                back();
-
-            $(AppiumBy.id("ru.ozon.app.android:id/searchTv")).shouldHave(text("Искать на Ozon"));
-        });
-
-        step("Нажать на поле <Искать на озон>", () ->
-                $(AppiumBy.id("ru.ozon.app.android:id/searchBar")).click());
-
-        step("Ввести в поле текст <Рыбалка>", () -> {
-            $(AppiumBy.id("ru.ozon.app.android:id/search_src_text"))
-                    .setValue("Рыбалка");
-
-            $(AppiumBy.id("ru.ozon.app.android:id/titleTv"))
-                    .click();
-        });
-
-        step("Проверить совподения", () -> {
-            $(AppiumBy.id("ru.ozon.app.android:id/titleTv")).shouldHave(text("Рыбалка"));
-        });
+        stepsMobile
+                .openCatalog()
+                .changeRegionIfExpressPos()
+                .skipBanner()
+                .checkSearchLine()
+                .search("Рыбалка")
+                .selectFirstPoint()
+                .checkMatches("Рыбалка");
     }
 
     @Test
     @DisplayName("Добавления товара в корзину")
     void addToCartAppTest() {
-        step("Нажать на кнопку <Каталог>", () ->
-                $(AppiumBy.id("ru.ozon.app.android:id/menu_catalog")).click());
-
-        step("Нажать на поле <Искать на озон>", () ->
-                $(AppiumBy.id("ru.ozon.app.android:id/searchBar")).click());
-
-        step("Ввести в поле текст <PS4 Игры>", () -> {
-            $(AppiumBy.id("ru.ozon.app.android:id/search_src_text"))
-                    .setValue("PS4 Игры");
-
-            $(AppiumBy.id("ru.ozon.app.android:id/titleTv"))
-                    .click();
-        });
-
-        step("Добавить первый товар из списка", () ->
-                $(AppiumBy.id("ru.ozon.app.android:id/primaryButtonContainer"))
-                        .click());
-
-        step("Перейти на вкладку <Корзина>", () ->
-                $(AppiumBy.id("ru.ozon.app.android:id/menu_cart"))
-                        .click());
-
-        step("Проверить корзину", () ->
-                $(AppiumBy.id("ru.ozon.app.android:id/titleTv"))
-                        .shouldHave(text("Товары (1)")));
+        stepsMobile
+                .changeRegionIfExpressPos()
+                .openCatalog()
+                .skipBanner()
+                .checkSearchLine()
+                .search("PS4 Игры")
+                .selectFirstPoint()
+                .addFirstProduct()
+                .openCart()
+                .checkCart("Товары (1)");
     }
 
     @ValueSource(strings = {"Воронеж", "Санкт-Петербург", "Новосибирск"})
     @ParameterizedTest(name = "Тестирование смены региона <{0}>")
     void changeRegionAppTest(String region) {
-        step("Нажать на кнопку <Moй OZON>", () ->
-                $(AppiumBy.id("ru.ozon.app.android:id/menu_profile")).click());
-
-        step("Нажать на кнопку локации", () ->
-                $(AppiumBy.id("ru.ozon.app.android:id/sectionIv")).click());
-
-        step("Вставить название города <" + region + ">", () ->
-                $(AppiumBy.id("ru.ozon.app.android:id/search_src_text"))
-                        .setValue(region));
-
-        step("Вставить название города <" + region + ">", () ->
-                $(AppiumBy.id("ru.ozon.app.android:id/listRv"))
-                        .$$(AppiumBy.id("ru.ozon.app.android:id/titleTv"))
-                        .get(1).click());
-
-        step("Обновить приложение", () -> {
-            back();
-            $(AppiumBy.id("ru.ozon.app.android:id/menu_profile")).click();
-        });
-
-        step("Проверить регион", () -> {
-            $(AppiumBy.id("ru.ozon.app.android:id/sectionTitleTv"))
-                    .shouldHave(text(region));
-        });
+        stepsMobile
+                .openProfile()
+                .clickRegionLocation()
+                .searchRegionName(region)
+                .selectRegionFromList()
+                .refresh()
+                .checkRegion(region);
     }
 
     @CsvSource(value = {
@@ -117,26 +63,12 @@ public class AndroidOzonTest extends TestBase {
             "Автотовары, Шины и диски"})
     @ParameterizedTest(name = "Тестирование выбора категорий <{0} - {1}>")
     void selectCategoryAppTest(String category, String subcategory) {
-        step("Нажать на кнопку <Каталог>", () ->
-                $(AppiumBy.id("ru.ozon.app.android:id/menu_catalog")).click());
-
-        step("Выбрать категорию <" + category + ">", () -> {
-            Selenide.sleep(2000);
-            if ($(AppiumBy.accessibilityId("widget common.pageHeader")).isDisplayed())
-                back();
-
-            $(AppiumBy.xpath(String.format("//android.widget.TextView[@text=\"%s\"]", category))).click();
-        });
-
-        step("Проверить выбранную категорию <" + category + ">", () -> {
-            Selenide.sleep(2000);
-            if ($(AppiumBy.accessibilityId("widget common.pageHeader")).isDisplayed())
-                back();
-
-            $(AppiumBy.xpath(String.format("//android.widget.TextView[@text=\"%s\"]", category))).shouldHave(text(category));
-        });
-
-        step("Проверить подкатегорию <" + subcategory + ">", () ->
-                $(AppiumBy.xpath(String.format("//android.widget.TextView[@text=\"%s\"]", subcategory))).shouldHave(text(subcategory)));
+        stepsMobile
+                .openCatalog()
+                .changeRegionIfExpressPos()
+                .selectCategory(category)
+                .skipBanner()
+                .checkCategory(category)
+                .checkSubcategory(subcategory);
     }
 }
---
+++ src/test/java/tests/mobile/steps/StepsMobile.java
@@ -0,0 +1,152 @@
+package tests.mobile.steps;
+
+import com.codeborne.selenide.Selenide;
+import io.appium.java_client.AppiumBy;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.back;
+
+public class StepsMobile {
+
+    private static final String
+            TITLE_TV = "ru.ozon.app.android:id/titleTv",
+            SEARCH_TV = "ru.ozon.app.android:id/searchTv",
+            EXPRESS_MAIN = "ru.ozon.app.android:id/menu_express_main";
+
+
+    @Step("Пропустить вспылвающую рекламу")
+    public StepsMobile skipBanner() {
+        Selenide.sleep(3000);
+        if ($(AppiumBy.accessibilityId("widget common.pageHeader")).isDisplayed())
+            back();
+        if ($(AppiumBy.accessibilityId("widget cms.banner")).isDisplayed())
+            back();
+        return this;
+    }
+
+    @Step("Сменить регион если каталог -> express")
+    public StepsMobile changeRegionIfExpressPos() {
+        if ($(AppiumBy.id(EXPRESS_MAIN)).isDisplayed()) {
+            skipBanner();
+            openProfile();
+            clickRegionLocation();
+            searchRegionName("Воронеж");
+            back();
+            openCatalog();
+        }
+        return this;
+    }
+
+    @Step("Обновить приложение")
+    public StepsMobile refresh() {
+        back();
+        $(AppiumBy.id("ru.ozon.app.android:id/menu_profile")).click();
+        return this;
+    }
+
+    @Step("Нажать на кнопку <Каталог>")
+    public StepsMobile openCatalog() {
+        $(AppiumBy.id("ru.ozon.app.android:id/menu_catalog")).click();
+        return this;
+    }
+
+    @Step("Выбрать категорию <{0}}>")
+    public StepsMobile selectCategory(String category) {
+        $(AppiumBy.xpath(String.format("//android.widget.TextView[@text=\"%s\"]", category))).click();
+        return this;
+    }
+
+    @Step("Проверить выбранную категорию <{0}}>")
+    public StepsMobile checkCategory(String category) {
+        $(AppiumBy.xpath(String.format("//android.widget.TextView[@text=\"%s\"]", category))).shouldHave(text(category));
+        return this;
+    }
+
+    @Step("Проверить подкатегорию <{0}}>")
+    public StepsMobile checkSubcategory(String subcategory) {
+        $(AppiumBy.xpath(String.format("//android.widget.TextView[@text=\"%s\"]", subcategory))).shouldHave(text(subcategory));
+        return this;
+    }
+
+    @Step("Нажать на кнопку <Карзину>")
+    public StepsMobile openCart() {
+        $(AppiumBy.id("ru.ozon.app.android:id/menu_cart")).click();
+        return this;
+    }
+
+    @Step("Нажать на кнопку <Moй OZON>")
+    public StepsMobile openProfile() {
+        $(AppiumBy.id("ru.ozon.app.android:id/menu_profile")).click();
+        return this;
+    }
+
+    @Step("Нажать на кнопку локации")
+    public StepsMobile clickRegionLocation() {
+        $(AppiumBy.id("ru.ozon.app.android:id/sectionIv")).click();
+        return this;
+    }
+
+    @Step("Вставить название города <{0}>")
+    public StepsMobile searchRegionName(String region) {
+        $(AppiumBy.id("ru.ozon.app.android:id/search_src_text"))
+                .setValue(region);
+        return this;
+    }
+
+    @Step("Выбрать город <{0}> из списка")
+    public StepsMobile selectRegionFromList() {
+        $(AppiumBy.id("ru.ozon.app.android:id/listRv"))
+                .$$(AppiumBy.id("ru.ozon.app.android:id/titleTv"))
+                .get(1)
+                .click();
+        return this;
+    }
+
+    @Step("Проверить наличие строки <Поиск>")
+    public StepsMobile checkSearchLine() {
+            $(AppiumBy.id(SEARCH_TV)).shouldHave(text("Искать на Ozon"));
+        return this;
+    }
+
+    @Step("Ввести в поле текст <{0}>")
+    public StepsMobile search(String value) {
+        $(AppiumBy.id("ru.ozon.app.android:id/searchBar")).click();
+        $(AppiumBy.id("ru.ozon.app.android:id/search_src_text"))
+                .setValue(value);
+        return this;
+    }
+
+    @Step("Выбрать первую позицию в поиске")
+    public StepsMobile selectFirstPoint() {
+        $(AppiumBy.id(TITLE_TV))
+                .click();
+        return this;
+    }
+
+    @Step("Проверить совподения c <{0}>")
+    public StepsMobile checkMatches(String value) {
+        $(AppiumBy.id(TITLE_TV)).shouldHave(text(value));
+        return this;
+    }
+
+    @Step("Добавить первый товар из списка")
+    public StepsMobile addFirstProduct() {
+        $(AppiumBy.id("ru.ozon.app.android:id/primaryButtonContainer")).click();
+        return this;
+    }
+
+    @Step("Проверить корзину по значнию")
+    public StepsMobile checkCart(String value) {
+        $(AppiumBy.id(TITLE_TV)).shouldHave(text(value));
+        return this;
+    }
+
+    @Step("Проверить регион <{0}>")
+    public StepsMobile checkRegion(String region) {
+        $(AppiumBy.id("ru.ozon.app.android:id/sectionTitleTv"))
+                .shouldHave(text(region));
+        return this;
+    }
+}
